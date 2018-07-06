37279d5d31181f23b8893fb04bcab33a3b037f90
Ara Adkins <me@ara.io>
2018-07-06 15:09:40 +0100

    Prepare for simple shell.

--- shell/src/Main.hs
+++ shell/src/Main.hs
@@ -5,6 +5,16 @@ import Prologue
 import qualified Luna.Shell.Command                as Command
 import qualified Luna.Shell.Option                 as Option
 
+-- TODO [Ara] Resolve project paths (including stdlib, local libraries, source).
+-- TODO [Ara] Execute a set of passes (static set for now).
+-- TODO [Ara] Execute the results.
+-- TODO [Ara] Gracefully display errors (once TC integrated).
+-- TODO [Ara] Edit project module to help.
+
+-- TODO [Ara] One single command `luna run` for now. If in project it runs the
+-- project, else expects a path (to package or standalone file)
+-- TODO [Ara] Check provided FOLDER is actually a project.
+
 ------------------
 -- === Main === --
 ------------------
